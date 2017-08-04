677a4df1502cd1b031949da67bac554e47c84b6e
jbochi <jbochi@gmail.com>
2017-08-04 15:10:58 -0400

    Add benchmark test

--- app_test.go
+++ app_test.go
@@ -20,3 +20,26 @@ func TestModel(t *testing.T) {
 		t.Errorf("Wrong number of recommendations: %v", recs)
 	}
 }
+
+func BenchmarkModel(b *testing.B) {
+	model, err := ReadModel("./data/")
+	if err != nil {
+		b.Fatalf("Unable to read model: %v", err)
+	}
+	if model == nil {
+		b.Fatalf("Did not return a model")
+	}
+	var recs []RepositoryScore
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		recs, err = model.Recommend([]string{"tensorflow/tensorflow", "BVLC/caffe"}, 10)
+	}
+
+	if err != nil {
+		b.Errorf("Failed to recommend: %s", err)
+	}
+	if len(recs) != 10 {
+		b.Errorf("Wrong number of recommendations: %v", recs)
+	}
+}
